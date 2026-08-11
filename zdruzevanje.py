22d8f7f7474c66e652e9e474b7d9050a5fad8857
Vita Bele <vitabele55@gmail.com>
2026-08-11 09:41:33 +0200

    združitev tabel

---
+++ zdruzevanje.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+df = pd.read_csv("igralke.csv")
+df_profili = pd.read_csv("profili.csv")
+
+print(df.head()) #.head je samo prvih nekaj profilov v oklepaju bi lahk dodali tudi koliko vrstice želimo
+print(df_profili.head())
+
+df["PovezavaIme"] = df["Ime"].str.lower().str.replace(" ", "-")
+
+print(df[["Ime", "PovezavaIme"]].head(2))
+
+df_koncni = df.merge(df_profili, left_on="PovezavaIme", right_on="PovezavaIme", how="left") #how lweft da obdži levi stolpec
+
+df_koncni = df_koncni.drop(columns=["PovezavaIme"]) #odstranimo stolpec povezava ime 
+
+
+print(df_koncni.head(2))
+print("Število vrstic:", len(df_koncni))
+
+
+df_koncni.to_csv("wta_podatki_koncni.csv", index=False)
+print("Shranjeno v wta_podatki_koncni.csv")
