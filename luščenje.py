13e85cc4c6a8d69cd479cd9dac4347b340e76bbc
Vita Bele <vitabele55@gmail.com>
2026-08-19 12:21:38 +0200

    izboljšava, pisanje funkcij

--- luščenje.py
+++ luščenje.py
@@ -1,37 +1,64 @@
-import requests
+#import requests
+#import re
+#import os
+#import pandas as pd
+#
+#with open("shranjene_strani/lestvica.html", "r", encoding="utf-8") as dat:
+#    podatki_html = dat.read()
+#
+#
+#imena = re.findall(r'data-player-name="([^"]+)"', podatki_html)
+#print("stevilo_imen:", len(imena)) #ker napiše 100 se vrjetno vsako ime podvoji zato s spodnjimi pregledamo in res dvakrat vsa imena
+##print(imena[:5])
+##print(imena[50:55])
+##popravimo
+#unikatna_imena = list(dict.fromkeys(imena))#list da pretvorimo nazaj v seznam, dict.fromkeys slovarji po definiciji nimajo ponavljanj, zato usako ime enkrat
+#print("stevilo unikatnih imen:", len(unikatna_imena))
+#print(unikatna_imena[:5])
+#
+#
+#tocke = re.findall(r'players-list__points[^>]*>\s*(\d+)', podatki_html)
+#print("Število točk:", len(tocke))
+#print(tocke[:5])
+#
+#drzave = re.findall(r'players-list__country--([A-Z]+)', podatki_html)
+#print("Število držav:", len(drzave))
+#print(drzave[:5])
+#
+#df = pd.DataFrame({
+#    "Ime": unikatna_imena,
+#    "Drzava": drzave,
+#    "Tocke": tocke
+#}) #data frame je tabela, vsebino podamo kot slovar
+#
+#print(df)
+#df.to_csv("igralke.csv", index=False) #csv - comma separeted values, index =false je to da ne sprinta nteksa
+#print("Shranjeno v igralke.csv")
+
 import re
-import os
 import pandas as pd
 
-with open("shranjene_strani/lestvica.html", "r", encoding="utf-8") as dat:
-    podatki_html = dat.read()
+
+def osnovni_podatki():
+    with open("shranjene_strani/lestvica.html", "r", encoding="utf-8") as dat:
+        html = dat.read()
+
+    imena = re.findall(r'data-player-name="([^"]+)"', html)
+    imena_unikatna = list(dict.fromkeys(imena))
+
+    tocke = re.findall(r'players-list__points[^>]*>\s*(\d+)', html)
+    drzave = re.findall(r'players-list__country--([A-Z]+)', html)
+
+    df = pd.DataFrame({
+        "Ime": imena_unikatna,
+        "Drzava": drzave,
+        "Tocke": tocke
+    })
+
+    df.to_csv("igralke.csv", index=False)
+    print("Osnovni podatki shranjeni v igralke.csv")
+    return df
 
 
-imena = re.findall(r'data-player-name="([^"]+)"', podatki_html)
-print("stevilo_imen:", len(imena)) #ker napiše 100 se vrjetno vsako ime podvoji zato s spodnjimi pregledamo in res dvakrat vsa imena
-#print(imena[:5])
-#print(imena[50:55])
-#popravimo
-unikatna_imena = list(dict.fromkeys(imena))#list da pretvorimo nazaj v seznam, dict.fromkeys slovarji po definiciji nimajo ponavljanj, zato usako ime enkrat
-print("stevilo unikatnih imen:", len(unikatna_imena))
-print(unikatna_imena[:5])
-
-
-tocke = re.findall(r'players-list__points[^>]*>\s*(\d+)', podatki_html)
-print("Število točk:", len(tocke))
-print(tocke[:5])
-
-drzave = re.findall(r'players-list__country--([A-Z]+)', podatki_html)
-print("Število držav:", len(drzave))
-print(drzave[:5])
-
-df = pd.DataFrame({
-    "Ime": unikatna_imena,
-    "Drzava": drzave,
-    "Tocke": tocke
-}) #data frame je tabela, vsebino podamo kot slovar
-
-print(df)
-df.to_csv("igralke.csv", index=False) #csv - comma separeted values, index =false je to da ne sprinta nteksa
-print("Shranjeno v igralke.csv")
-
+if __name__ == "__main__":
+    osnovni_podatki()
--- prenos.py
+++ prenos.py
@@ -23,11 +23,6 @@ print("Sabalenka" in odgovor.text ) #preverim, če so se prenesla imena
 #Rang (mesto na lestvici): <div class="players-list__rank">
 print(odgovor.text.count("players-list__item")) #da vidimo koliko imen je prekopiralo
 
-os.makedirs("shranjene_strani", exist_ok= True) #naredi mapico shranjene strani, ta del exist=ok je zato da ni napake če je mapica že torej lahko večkrat poženemo kodo
-with open("shranjene_strani/lestvica.html", "w", encoding="utf-8") as dat:
-    dat.write(odgovor.text)
-    print('napisano')
-
 imena = re.findall(r'data-player-name="([^"]+)"', odgovor.text)
 print("stevilo_imen:", len(imena)) #ker napiše 100 se vrjetno vsako ime podvoji zato s spodnjimi pregledamo in res dvakrat vsa imena
 print(imena[:5])
@@ -40,3 +35,22 @@ print(unikatna_imena[:5])
 točke = tocke = re.findall(r'players-list__points">(\d+)', odgovor.text) #del /d+ je da so samo številke 
 print("Število točk:", len(tocke))
 print(tocke[:5])
+
+def prenesi_lestvico():
+    URL = 'https://www.wtatennis.com/players'
+    HEADERS ={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"}
+#zato da ne mislijo da smo robot (pridobljeno: https://www.geeksforgeeks.org/python/user-agent-in-python-request/)
+    odgovor = requests.get(URL, headers=HEADERS)
+#obisk spletne strani, pošlje get ( The get() method sends a GET request to the specified url.)
+    os.makedirs("shranjene_strani", exist_ok= True) #naredi mapico shranjene strani, ta del exist=ok je zato da ni napake če je mapica že torej lahko večkrat poženemo kodo
+    pot_do_datoteke = "shranjene_strani/lestvica.html"
+    if not os.path.exists(pot_do_datoteke):
+        odgovor = requests.get(URL, headers=HEADERS)
+        with open("shranjene_strani/lestvica.html", "w", encoding="utf-8") as dat:
+            dat.write(odgovor.text)
+        print('napisano')
+    else:
+        print("Lestvica že obstaja.")
+
+if __name__ == "__main__":
+    prenesi_lestvico()
